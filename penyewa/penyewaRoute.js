0c48b9d8d0020f17ae1407b8791cc14769e4007b
ramagiox <32376657+ramagiox@users.noreply.github.com>
2017-10-31 11:48:05 +0700

    Update penyewaRoute.js

--- penyewa/penyewaRoute.js
+++ penyewa/penyewaRoute.js
@@ -1,6 +1,30 @@
 let ex = require('express');
 let route = ex.Router();
 let penyewaController = require('./penyewaController.js')
+let User = require('./penyewaModel.js')
+
+route.post('/login/authenticate', function(req, res){
+    let data = {
+        UserNamePenyewa: req.body.UserNamePenyewa,
+        PasswordPenyewa: req.body.PasswordPenyewa
+    };
+    User.findOne(data).lean().exec(function(err, user){
+        if(err){
+            return res.json({error: true});
+        }
+        if(!user){
+            return res.status(404).json({'message':'User not found!'});
+        }
+        console.log(user);
+        
+        // let token = jwt.sign(user, global.config.jwt_secret, {
+        //     expiresIn: 1440 // expires in 1 hour
+        // });
+        // //console.log(token);
+        
+        res.json(user.UserNamePenyewa);
+    });
+});
 
 route.get('/penyewa', function (req, res) {
     penyewaController.getPenyewa(function (err, respon) {
